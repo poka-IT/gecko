012cd31daf00819eb3091b0a8db076fb7ec3bcc1
poka <poka@p2p.legal>
2021-11-27 07:48:35 +0100

    Remove trace of gva search adapter

--- lib/models/search.dart
+++ lib/models/search.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gecko/globals.dart';
 import 'package:gecko/models/g1_wallets_list.dart';
-import 'package:gecko/models/g1_wallets_list_live.dart';
 import 'package:http/http.dart' as http;
 
 class SearchProvider with ChangeNotifier {
@@ -40,45 +39,6 @@ class SearchProvider with ChangeNotifier {
         return;
       }
     });
-
-    return searchResult;
-  }
-
-  Future<List> searchBlockchainLive() async {
-    searchResult.clear();
-    int searchTime = DateTime.now().millisecondsSinceEpoch;
-
-    if (cacheTime + cacheDuring <= searchTime) {
-      g1WalletsBox.clear();
-      final url = Uri.parse(
-          'https://g1.librelois.fr/gva?query={%20wallets(pagination:%20{%20ord:%20ASC,%20pageSize:%20999%20})%20{%20pageInfo%20{%20hasNextPage%20endCursor%20}%20edges%20{%20node%20{%20script%20balance%20{%20amount%20base%20}%20idty%20{%20isMember%20username%20}%20}%20}%20}%20}');
-      final response = await http.get(url);
-      // log.d(response.body);
-
-      G1WalletsListLive _jsonResponse =
-          G1WalletsListLive.fromJson(json.decode(response.body));
-
-      while (_jsonResponse.data.wallets.pageInfo.hasNextPage) {
-        var cursor = _jsonResponse.data.wallets.pageInfo.endCursor;
-        final url = Uri.parse(
-            'https://g1.librelois.fr/gva?query={%20wallets(pagination:%20{%20ord:%20ASC,%20pageSize:%20999%20})%20{%20pageInfo%20{%20hasNextPage%20endCursor%20}%20edges%20{%20node%20{%20script%20balance%20{%20amount%20base%20}%20idty%20{%20isMember%20username%20}%20}%20}%20}%20}');
-        final response = await http.get(url);
-      }
-
-      await configBox.put('g1WalletCache', _jsonResponse);
-      cacheTime = DateTime.now().millisecondsSinceEpoch;
-    }
-
-    for (var value in configBox.get('g1WalletCache').data.wallets.edges) {
-      if ((value.node.idty != null &&
-              value.node.idty.username != null &&
-              value.node.idty.username.contains(searchController.text)) ||
-          value.node.script.contains(searchController.text)) {
-        searchResult.add(value);
-      }
-    }
-
-    // log.d(configBox.get('g1WalletCache').data.wallets.edges.toString());
 
     return searchResult;
   }

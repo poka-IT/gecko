668831424bd57089ed3fc810534dca6907070ecb
poka <poka@p2p.legal>
2024-12-24 15:07:08 +0100

    fix cert wait button rendering

--- lib/widgets/certify/wait_to_cert.dart
+++ lib/widgets/certify/wait_to_cert.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:gecko/globals.dart';
 import 'package:gecko/models/scale_functions.dart';
 import 'package:gecko/screens/wallet_view.dart' show buttonSize, buttonFontSize;
 
@@ -7,32 +8,24 @@ class WaitToCertWidget extends StatelessWidget {
   final String messageKey;
   final String duration;
 
-  const WaitToCertWidget(
-      {super.key, required this.messageKey, required this.duration});
+  const WaitToCertWidget({super.key, required this.messageKey, required this.duration});
 
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
       ScaledSizedBox(
         height: buttonSize,
-        child: Container(
-          foregroundDecoration: const BoxDecoration(
-            color: Colors.grey,
-            backgroundBlendMode: BlendMode.saturation,
-          ),
-          child: const Opacity(
-            opacity: 0.5,
-            child: Image(image: AssetImage('assets/gecko_certify.png')),
-          ),
+        child: Image(
+          image: const AssetImage('assets/gecko_certify.png'),
+          color: backgroundColor,
+          colorBlendMode: BlendMode.saturation,
+          opacity: const AlwaysStoppedAnimation<double>(0.5),
         ),
       ),
       Text(
         messageKey.tr(args: [duration]),
         textAlign: TextAlign.center,
-        style: scaledTextStyle(
-            fontSize: buttonFontSize - 4,
-            fontWeight: FontWeight.w400,
-            color: Colors.grey[600]),
+        style: scaledTextStyle(fontSize: buttonFontSize - 4, fontWeight: FontWeight.w400, color: Colors.grey[600]),
       ),
     ]);
   }

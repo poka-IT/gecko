f564da403500b1cb7dd3e705f36799dac278efa9
poka <poka@p2p.legal>
2026-03-11 17:55:31 +0100

    Fix Windows runner default window types

--- windows/runner/win32_window.h
+++ windows/runner/win32_window.h
@@ -13,15 +13,15 @@
 class Win32Window {
  public:
   struct Point {
-    unsigned int x;
-    unsigned int y;
-    Point(unsigned int x, unsigned int y) : x(x), y(y) {}
+    int x;
+    int y;
+    Point(int x, int y) : x(x), y(y) {}
   };
 
   struct Size {
-    unsigned int width;
-    unsigned int height;
-    Size(unsigned int width, unsigned int height)
+    int width;
+    int height;
+    Size(int width, int height)
         : width(width), height(height) {}
   };
 
